a3451962c71d395c9e763e3254cf6556e8c36117
casantiago <v-casantiago@expediagroup.com>
2022-11-29 16:26:33 -0600

    fix: missing changes

--- src/mappers/index.ts
+++
@@ -1,3 +0,0 @@
-export * from "./product";
-export * from "./extra";
-export * from "./extra-entry";
--- src/services/index.ts
+++ src/services/index.ts
@@ -1,16 +1,13 @@
 export * from './product';
 export * from './extra/extra';
-export * from './extra-entry';
 export * from './extra-section';
 
 import { productService } from "./product";
 import { extraService } from "./extra";
-import { extraEntryService } from "./extra-entry";
 import { extraSectionService } from "./extra-section";
 
 export default {
   productService,
   extraService,
-  extraEntryService,
   extraSectionService,
 };
